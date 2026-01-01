892689f8a6da51f59811d061eebbc1797af864fa
Farhan-Khan7 <fk5518744@gmail.com>
2026-01-02 01:16:14 +0530

    This is my 3rd commit of this year

--- code.py
+++ code.py
@@ -5,10 +5,14 @@ engine = pyttsx3.init()
 
 x = int(input("Enter value of x = "))
 y = int(input("Enter value of y = "))
+z = float(input("Enter value of z = "))
 
 
+avg = float((x+y+z)/3)
 
-print("Sum = " , x+y)
-print("Difference = " , x-y)
-print("product = " , x*y)
-print("quotient = ", x/y)
+print(type(avg))
+
+# print("Sum = " , x+y)
+# print("Difference = " , x-y)
+# print("product = " , x*y)
+# print("quotient = ", x/y)
