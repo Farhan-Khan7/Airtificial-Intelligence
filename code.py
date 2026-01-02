5fa0abad37848d3509034e3cd59152aa3b5977b8
Farhan-Khan7 <fk5518744@gmail.com>
2026-01-03 00:14:47 +0530

    I have done 2nd commit of todya

--- code.py
+++ code.py
@@ -3,12 +3,22 @@ engine = pyttsx3.init()
 
 
 
-age = int(input("Enter your Age = "))
+# age = int(input("Enter your Age = "))
 
 
-if age < 18:
-    print("Child")
-elif age == 18:
-    print("TeenAge")
-elif age > 18:
-    print("Adult")
+# if age < 18:
+#     print("Child")
+# elif age == 18:
+#     print("TeenAge")
+# elif age > 18:
+#     print("Adult")
+
+username = input("Enter Username : ")
+password = input("Enter Password : ")
+
+if(username == "admin" and password == "pass"):
+    print("Login Successfully !")
+elif(username != "admin"):
+    print("Username Wrong !")
+else:
+    print("Wrong Password !")
