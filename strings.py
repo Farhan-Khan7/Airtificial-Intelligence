4e562b1ff6ca245b63c83264d03f5fec62554b95
Farhan-Khan7 <fk5518744@gmail.com>
2026-01-10 12:58:06 +0530

    I have done string

--- strings.py
+++ strings.py
@@ -1,3 +1,5 @@
-string = "Harry"
-
-print(string[-4:-2])
+pie = "ApplePie"
+print(pie[:5])      #Slicing from Start
+print(pie[5:])      #Slicing till End
+print(pie[2:6])     #Slicing in between
+print(pie[-8:])     #Slicing using negative index
