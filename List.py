ac0de4c83139d0b676c0ae47f1e29b7c0940cab8
Farhan-Khan7 <fk5518744@gmail.com>
2026-01-11 23:20:37 +0530

    I have done

--- List.py
+++ List.py
@@ -45,7 +45,7 @@
 
 animals = ["cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow"]
 print(animals[::2])		#using positive indexes
-print(animals[-8:-1:2])	#using negative indexes
+print(animals[-8:-1:3])	#using negative indexes
 
 
 
