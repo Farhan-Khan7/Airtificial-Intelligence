975b3239777f335fd8d889f61f10e2540abf5636
Farhan-Khan7 <fk5518744@gmail.com>
2026-01-11 12:21:12 +0530

    I have done

--- List.py
+++ List.py
@@ -1,16 +1,39 @@
-colors = ["Red", "Green", "Blue", "Yellow", "Green"]
-#          [0]      [1]     [2]      [3]      [4]
+# colors = ["Red", "Green", "Blue", "Yellow", "Green"]
+# #          [0]      [1]     [2]      [3]      [4]
 
-print(colors[0])  # Output: Red
-print(colors[2])  # Output: Blue
-print(colors[4])  # Output: Green
-print(colors[-1]) # Output: Green
-print(colors[-3]) # Output: Blue
-print(colors[-5]) # Output: Red
-print(colors[1:4]) # Output: ['Green', 'Blue', 'Yellow']
-print(colors[:3])  # Output: ['Red', 'Green', 'Blue']
-print(colors[2:])  # Output: ['Blue', 'Yellow', 'Green']
-print(colors[-4:-1]) # Output: ['Green', 'Blue', 'Yellow']
-print(colors[-3:])   # Output: ['Blue', 'Yellow', 'Green']  
-print(colors[:-2])  # Output: ['Red', 'Green', 'Blue']
-print(colors[:])    # Output: ['Red', 'Green', 'Blue', 'Yellow', 'Green']
+# print(colors[0])  # Output: Red
+# print(colors[2])  # Output: Blue
+# print(colors[4])  # Output: Green
+# print(colors[-1]) # Output: Green
+# print(colors[-3]) # Output: Blue
+# print(colors[-5]) # Output: Red
+# print(colors[1:4]) # Output: ['Green', 'Blue', 'Yellow']
+# print(colors[:3])  # Output: ['Red', 'Green', 'Blue']
+# print(colors[2:])  # Output: ['Blue', 'Yellow', 'Green']
+# print(colors[-4:-1]) # Output: ['Green', 'Blue', 'Yellow']
+# print(colors[-3:])   # Output: ['Blue', 'Yellow', 'Green']  
+# print(colors[:-2])  # Output: ['Red', 'Green', 'Blue']
+# print(colors[:])    # Output: ['Red', 'Green', 'Blue', 'Yellow', 'Green']
+
+
+# colors = ["Red", "Green", "Blue", "Yellow", "Green"]
+# if "Yellow" in colors:
+#     print("Yellow is present.")
+# else:
+#     print("Yellow is absent.")
+
+
+# colors = ["Red", "Green", "Blue", "Yellow", "Green"]
+# for colorsval in colors:
+#     print(colorsval)
+
+
+# animals = ["cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow"]
+# print(animals[4:])	#using positive indexes
+# print(animals[-4:])	#using negative indexes
+
+
+
+animals = ["cat", "dog", "bat", "mouse", "pig", "horse", "donkey", "goat", "cow"]
+print(animals[:6])	#using positive indexes
+print(animals[:-3])	#using negative indexes
