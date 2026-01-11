778d9525f3a3007cd6fefbf318bc60c8da10b23a
Farhan-Khan7 <fk5518744@gmail.com>
2026-01-11 12:10:10 +0530

    To day First Commit

---
+++ List.py
@@ -0,0 +1,16 @@
+colors = ["Red", "Green", "Blue", "Yellow", "Green"]
+#          [0]      [1]     [2]      [3]      [4]
+
+print(colors[0])  # Output: Red
+print(colors[2])  # Output: Blue
+print(colors[4])  # Output: Green
+print(colors[-1]) # Output: Green
+print(colors[-3]) # Output: Blue
+print(colors[-5]) # Output: Red
+print(colors[1:4]) # Output: ['Green', 'Blue', 'Yellow']
+print(colors[:3])  # Output: ['Red', 'Green', 'Blue']
+print(colors[2:])  # Output: ['Blue', 'Yellow', 'Green']
+print(colors[-4:-1]) # Output: ['Green', 'Blue', 'Yellow']
+print(colors[-3:])   # Output: ['Blue', 'Yellow', 'Green']  
+print(colors[:-2])  # Output: ['Red', 'Green', 'Blue']
+print(colors[:])    # Output: ['Red', 'Green', 'Blue', 'Yellow', 'Green']
