94ada22321c9e9e5b0d7e715e10026e897e23643
Farhan-Khan7 <fk5518744@gmail.com>
2026-01-12 22:34:21 +0530

    I have done

---
+++ tuple.py
@@ -0,0 +1,20 @@
+# countries = ("Spain", "Italy", "India", "England", "Germany")
+# #tuples are immutable, so we convert to list to modify
+# temp = list(countries)
+# temp.append("Russia")       #add item 
+# temp.pop(3)                 #remove item
+# temp[2] = "Finland"         #change item
+# countries = tuple(temp)
+# print(countries)
+
+
+countries = ("Pakistan", "Afghanistan", "Bangladesh", "ShriLanka")
+countries2 = ("Vietnam", "India", "China")
+# southEastAsia = countries + countries2
+# print(southEastAsia)
+
+countries3 = list(countries)
+
+print(countries3)
+southEastAsia = countries3 + list(countries2)
+print(southEastAsia)
