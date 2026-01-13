c7d578f55c2b2103e8cdf5c8bca74a0318b2ee6b
Farhan-Khan7 <fk5518744@gmail.com>
2026-01-13 23:54:16 +0530

    I have done

--- tuple.py
+++ tuple.py
@@ -8,8 +8,8 @@
 
 countries = ("Pakistan", "Afghanistan", "Bangladesh", "ShriLanka")
 countries2 = ("Vietnam", "India", "China")
-# southEastAsia = countries + countries2
-# print(southEastAsia)
+southEastAsia = countries + countries2
+print(southEastAsia)
 
 # countries3 = list(countries)
 
