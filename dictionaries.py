82937635be0becc0c6115645fd46ba40e253fde1
Farhan-Khan7 <fk5518744@gmail.com>
2026-01-15 00:41:35 +0530

    I have create new file for dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,9 @@
+info = {
+    'author': 'Airtificial Intelligence',
+    'version': '1.0.0',
+    'description': 'A collection of dictionaries for various applications.',
+    'license': 'MIT',
+}
+
+print(info['author'])  # Output: Airtificial Intelligence
+print(info.get('version'))  # Output: 1.0.0
