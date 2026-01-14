37f796d6ac89f9f7af7b68dc72a360f268f5cfba
Farhan-Khan7 <fk5518744@gmail.com>
2026-01-14 23:55:38 +0530

    I have done

--- sets.py
+++ sets.py
@@ -16,9 +16,17 @@ print(f"S3 = {s3}\nS4 = {s4} \nS5 = {s5}")
 
 
 s1.add(8)
-s2.remove(7)
+# s2.remove(7)
 
 print(f"S1 = {s1} \nS2 = {s2} ")
 
 s1.update([9, 10])
 print(f"S1 = {s1} ")
+
+
+s2.discard(66)
+print(f"S2 = {s2} ")
+
+
+# s2.remove(35)
+print(f"S2 = {s2} ")
