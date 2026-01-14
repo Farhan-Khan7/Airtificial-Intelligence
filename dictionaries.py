b4d6cb3d54c820a7dea8537abbadd67d47d1e464
Farhan-Khan7 <fk5518744@gmail.com>
2026-01-15 00:45:44 +0530

    I have done some operation

--- dictionaries.py
+++ dictionaries.py
@@ -5,5 +5,12 @@ info = {
     'license': 'MIT',
 }
 
-print(info['author'])  # Output: Airtificial Intelligence
-print(info.get('version'))  # Output: 1.0.0
+# print(info['author'])  # Output: Airtificial Intelligence
+# print(info.get('version'))  # Output: 1.0.0
+
+dict_keys = info.keys()
+dict_values = info.values()
+
+print(dict_keys)    # Output: ['author', 'version', 'description', 'license']
+print(dict_values)  # Output: ['Airtificial Intelligence', '1.0.0', 'A collection of dictionaries for various applications.', 'MIT']
+
