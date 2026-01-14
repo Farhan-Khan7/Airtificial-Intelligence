0706e00150d635c32ba1c5ace8d90f4693683c6b
Farhan-Khan7 <fk5518744@gmail.com>
2026-01-14 23:53:26 +0530

    I have done

--- sets.py
+++ sets.py
@@ -13,3 +13,12 @@ s4 = (s1.intersection(s2))
 s5 = (s1.symmetric_difference(s2))
 
 print(f"S3 = {s3}\nS4 = {s4} \nS5 = {s5}")
+
+
+s1.add(8)
+s2.remove(7)
+
+print(f"S1 = {s1} \nS2 = {s2} ")
+
+s1.update([9, 10])
+print(f"S1 = {s1} ")
