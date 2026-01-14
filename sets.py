8153636cccb386937124f091a33709f535d2d940
Farhan-Khan7 <fk5518744@gmail.com>
2026-01-14 23:38:36 +0530

    I have done

--- sets.py
+++ sets.py
@@ -5,8 +5,11 @@ print(type(set))
 s1 = {1, 2, 3, 4}
 s2 = {4, 5, 6 , 7}
 
-print(s1.union(s2))
-print(s1.intersection(s2))
+print(f"S1 = {s1} \nS2 = {s2} ")
 
 
-# print(f"S1 = {s1} \nS2 = {s2} ")
+s3 = (s1.union(s2))
+s4 = (s1.intersection(s2))
+s5 = (s1.symmetric_difference(s2))
+
+print(f"S3 = {s3}\nS4 = {s4} \nS5 = {s5}")
