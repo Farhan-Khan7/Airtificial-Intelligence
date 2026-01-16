232d160a18a3b9b9b2d5221932403321f9aa786d
Farhan-Khan7 <fk5518744@gmail.com>
2026-01-16 22:51:03 +0530

    I have done

--- conditional.py
+++ conditional.py
@@ -21,19 +21,18 @@
 # print(f"Ticket price for you is {price}$")
 
 
-score = 59
+# score = 59
 
-
-if score >= 90:
-    grade = 'A'
-elif score >= 80:
-    grade = 'B'
-elif score >= 70:
-    grade = 'C'
-elif score >= 60:
-    grade = 'D' 
-else:
-    grade = 'Failed'
+# if score >= 90:
+#     grade = 'A'
+# elif score >= 80:
+#     grade = 'B'
+# elif score >= 70:
+#     grade = 'C'
+# elif score >= 60:
+#     grade = 'D' 
+# else:
+#     grade = 'Failed'
     
     
-print(grade)
+# print(grade)
