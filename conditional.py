1824d81098ebbd1fbcb2c4e51b8a3863c42e5208
Farhan-Khan7 <fk5518744@gmail.com>
2026-01-16 22:50:28 +0530

    I have done

--- conditional.py
+++ conditional.py
@@ -10,3 +10,30 @@
 #     print("Senior !")
 
 
+# age = 18
+# wednesday = True
+
+# price = 12 if age >= 18 else 8 
+
+# if wednesday == True:
+#     price -= 2
+
+# print(f"Ticket price for you is {price}$")
+
+
+score = 59
+
+
+if score >= 90:
+    grade = 'A'
+elif score >= 80:
+    grade = 'B'
+elif score >= 70:
+    grade = 'C'
+elif score >= 60:
+    grade = 'D' 
+else:
+    grade = 'Failed'
+    
+    
+print(grade)
